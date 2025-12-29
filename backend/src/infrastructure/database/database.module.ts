528129a3abf4a3859be79fb7808fc76582eb7807
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-12-29 14:57:10 -0300

    feat: adding database module

---
+++ backend/src/infrastructure/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: Number(config.get('DB_PORT')),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false, 
+        logging: config.get('NODE_ENV') === 'development',
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
