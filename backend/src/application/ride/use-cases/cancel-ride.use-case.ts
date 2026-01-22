e045c7e2716bde87a36fdea0e3960b1c0345d288
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2026-01-22 13:24:59 -0300

    feat: building cancel ride use case

---
+++ backend/src/application/ride/use-cases/cancel-ride.use-case.ts
@@ -0,0 +1,40 @@
+import { BadRequestException, ForbiddenException, Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { RESPONSES } from "src/core/response/response.messages";
+import { mapDomainErrorToHttp } from "src/domain/errors/http-error.mapper";
+import type { BookingsRepository } from "src/domain/repositories/bookings.repository";
+import { BOOKINGS_REPOSITORY, RIDES_REPOSITORY } from "src/domain/repositories/repository.tokens";
+import type { RidesRepository } from "src/domain/repositories/rides.repository";
+
+export interface CancelRideRequest {
+    rideId: string,
+    userId: string,
+}
+
+@Injectable()
+export class CancelRideUseCase {
+    constructor(
+        @Inject(RIDES_REPOSITORY)
+        private readonly ridesRepository: RidesRepository,
+    ) {}
+
+    async execute(request: CancelRideRequest) {
+        const {
+            rideId,
+            userId,
+        } = request;
+
+        const ride = await this.ridesRepository.findByIdWithBookings(rideId);
+
+        if (!ride) {
+            throw new NotFoundException(RESPONSES.RIDES.NOT_FOUND);
+        }
+
+        try {
+            ride.cancel(userId);
+            await this.ridesRepository.save(ride);
+        } catch (error) {
+            mapDomainErrorToHttp(error);
+        }
+
+    }
+}
